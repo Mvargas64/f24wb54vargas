d053470aed7ccca41ced4596817f1b5a3d10b03a
Mario Vargas <113541977+Mvargas64@users.noreply.github.com>
2024-11-14 14:49:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -95,9 +95,9 @@ app.use('/users', usersRouter);
 app.use('/grid', gridRouter);
 
 // Start the server on port 3000
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+//app.listen(3000, () => {
+//  console.log("Server is running on http://localhost:3000");
+//});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
